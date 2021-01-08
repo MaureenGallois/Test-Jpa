6ef68b2c2b3cfa0b6d445d6efb14fffd41761940
Maureen GALLOIS <m.gallois@unyc.io>
2021-01-08 11:26:07 +0100

    tests connexion terminé

---
+++ src/main/java/TestJpa.java
@@ -0,0 +1,9 @@
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestJpa {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu-test-jpa");
+        emf.close();
+    }
+}
